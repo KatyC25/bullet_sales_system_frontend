4475040df28424f4d25344f83c8969b891be9dba
Katherine <cundanok9@gmail.com>
2022-08-31 18:25:11 -0600

    pagina login

---
+++ components/Form.js
@@ -0,0 +1,44 @@
+export default function Form() {
+    return (
+        <div className='bg-white px-10 py-20 rounded-3xl border-gray-100'>
+            <h1 className='text-5xl font-semibold'>Bienvenido de vuelta</h1>
+            <p className='font-medium text-lg text-gray-500 mt-4'> Bienvenido de vuelta! Por favor introduce tus datos.</p>
+            <div className='mt-8'>
+                <div >
+                    <label className='text-lg font-medium'> Correo </label>
+                    <input
+                        className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                        placeholder='Introduce tu Correo Electronico'
+                    />
+
+                </div>
+                <div>
+
+                    <label className='text-lg font-medium'> Contraseña</label>
+                    <input
+                        className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                        placeholder='Introduce tu Contraseña'
+                    />
+
+                </div>
+                <div className='mt-8 flex justify-between items-center '>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id='remember'
+                        />
+                        <label className='ml-2 font-medium text-base' for="remember"> Recordar por 30 dias </label>
+                    </div>
+
+
+                </div>
+                <div className='mt-8 flex-col gap-y-4'>
+                    <button className='active:scale-[.98] w-full active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold'> Entrar</button>
+
+                </div>
+                <p> </p>
+            </div>
+
+        </div>
+    )
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Login from "./example"
 
 export default function Home() {
   return (
     <div>
-      <h1 className="m-[32rem] bg-blue-400 py-6 rounded-2xl text-center text-white text-3xl" >
-        Hola Mundo!
+      <h1 className="" >
+        <Login />
       </h1>
     </div>
   )
